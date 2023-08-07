21d4073eaba7cce2219e15e4b0cb20af700ec709
datajcthemax <data.jcthemax@gmail.com>
2023-08-07 16:22:21 +0900

    prisma schema done and db connected

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -88,6 +88,7 @@ const AuthForm = () => {
                             label="Name"
                             register={register}
                             errors={errors}
+                            disabled={isLoading}
                         />
                     )}
                      <Input 
@@ -96,6 +97,7 @@ const AuthForm = () => {
                         type="email"
                         register={register}
                         errors={errors}
+                        disabled={isLoading}
                     />
                      <Input 
                         id="password" 
@@ -103,6 +105,7 @@ const AuthForm = () => {
                         type="password"
                         register={register}
                         errors={errors}
+                        disabled={isLoading}
                     />
                     <div>
                         <Button
