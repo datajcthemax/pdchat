f69432bb7eac863dc3c26ef65563d8e4c265e3d7
datajcthemax <data.jcthemax@gmail.com>
2023-08-07 12:58:48 +0900

    form input midway

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import { useCallback, useState } from "react";
+import { 
+    FieldValues, 
+    SubmitHandler, 
+    useForm 
+} from "react-hook-form";
+
+import Input from "@/app/components/inputs/Input";
+
+type Varaint = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+    const [variant, setVariant] = useState<Varaint>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(() => { 
+        if (variant === 'LOGIN') {
+            setVariant('REGISTER');
+        } else {
+            setVariant('LOGIN');
+        }
+     }, [variant]);
+
+     const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        }
+     } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+     });
+
+     const onSubmit: SubmitHandler<FieldValues> = (data) => { 
+        setIsLoading(true);
+
+        if (variant === 'REGISTER') {
+            // Axios Register
+        }
+
+        if (variant === 'LOGIN') {
+            // NextAuth SignIn
+        }
+      }
+
+      const socialAction = (action: string) => { 
+        setIsLoading(true);
+
+        // NextAuth Social Sign In
+       }
+
+    return ( 
+        <div 
+            className="
+                mt-8
+                sm:mx-auto
+                sm:w-full
+                sm:max-w-md
+            "
+        >
+            <div
+                className="
+                    bg-white
+                    px-4
+                    py-8
+                    shadow
+                    sm:rounded-lg
+                    sm:px-10
+                "
+            >
+                <form
+                    className="space-y-6"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <Input 
+                        id="email" 
+                        label="Email"
+                        register={register}
+                    />
+                </form>
+            </div>
+        </div>
+     );
+}
+ 
+export default AuthForm;
