09a55cdf0bb67b97e56b6cd715122c70618a32f7
datajcthemax <data.jcthemax@gmail.com>
2023-08-10 11:28:11 +0900

    quick fix

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -44,83 +44,85 @@ const Modal: React.FC<ModalProps> = ({
                         "
                     />
                 </Transition.Child>
-                <div
-                    className="
-                        flex
-                        min-h-full
-                        items-center
-                        justify-center
-                        p-4
-                        text-center
-                        sm:p-0
-                    "
-                >
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out dration-300"
-                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        enterTo="opacity-100 translate-y-0 sm:scale-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div
+                        className="
+                            flex
+                            min-h-full
+                            items-center
+                            justify-center
+                            p-4
+                            text-center
+                            sm:p-0
+                        "
                     >
-                        <Dialog.Panel
-                            className="
-                                relative
-                                transform
-                                overflow-hidden
-                                rounded-lg
-                                bg-white
-                                px-4
-                                pb-4
-                                text-left
-                                shadow-xl
-                                transition-all
-                                w-full
-                                sm:my-8
-                                sm:w-full
-                                sm:max-w-lg
-                                sm:p-6
-                            "
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out dration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
-                            <div
+                            <Dialog.Panel
                                 className="
-                                    absolute
-                                    right-0
-                                    top-0
-                                    hidden
-                                    pr-4
-                                    pt-4
-                                    sm:block
-                                    z-10
+                                    relative
+                                    transform
+                                    overflow-hidden
+                                    rounded-lg
+                                    bg-white
+                                    px-4
+                                    pb-4
+                                    text-left
+                                    shadow-xl
+                                    transition-all
+                                    w-full
+                                    sm:my-8
+                                    sm:w-full
+                                    sm:max-w-lg
+                                    sm:p-6
                                 "
                             >
-                                <button
-                                    type="button"
+                                <div
                                     className="
-                                        rounded-md
-                                        bg-white
-                                        text-gray-400
-                                        hover:text-gray-500
-                                        focus:outline-none
-                                        focus:right-2
-                                        focus:ring-purple-500
-                                        focus:ring-offset-2
+                                        absolute
+                                        right-0
+                                        top-0
+                                        hidden
+                                        pr-4
+                                        pt-4
+                                        sm:block
+                                        z-10
                                     "
-                                    onClick={onClose}
                                 >
-                                    <span className="sr-only">닫기</span>
-                                    <IoClose 
-                                        clssName="
-                                        h-6
-                                        w-6
+                                    <button
+                                        type="button"
+                                        className="
+                                            rounded-md
+                                            bg-white
+                                            text-gray-400
+                                            hover:text-gray-500
+                                            focus:outline-none
+                                            focus:right-2
+                                            focus:ring-purple-500
+                                            focus:ring-offset-2
                                         "
-                                    />
-                                </button>
-                            </div>
-                            {children}
-                        </Dialog.Panel>
-                    </Transition.Child>
+                                        onClick={onClose}
+                                    >
+                                        <span className="sr-only">닫기</span>
+                                        <IoClose 
+                                            clssName="
+                                            h-6
+                                            w-6
+                                            "
+                                        />
+                                    </button>
+                                </div>
+                                {children}
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
                 </div>
             </Dialog>
         </Transition.Root>
